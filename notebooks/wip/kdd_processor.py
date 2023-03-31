c728ebec31c15e056eb60f5d0f7281bb69a8052f
Prayag Bhattarai <bhattaraiprayag@hotmail.com>
2023-03-31 14:14:14 +0200

    improved code descriptions and comments

--- notebooks/wip/kdd_processor.py
+++ notebooks/wip/kdd_processor.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 # Function to load and clean products data
 def load_and_clean_data(file_path, is_products=False, processed_file=None, slice_size=None):
+    """
+    This function loads and cleans data from the given file_path.
+    If the is_products flag is set to True, it fills missing values and removes special characters from the data.
+    If a processed file is specified and exists, it loads the data from there instead of the original file.
+    If a slice size is specified, it loads only a portion of the data.
+    """
     if is_products and processed_file and os.path.exists(processed_file):
         df = pd.read_csv(processed_file, nrows=slice_size) if slice_size else pd.read_csv(processed_file)
     else:
@@ -41,12 +47,16 @@ task = 'task1'
 train_path = '../../data/train/'
 test_path = '../../data/test/sessions_test_' + task + '.csv'
 PREDS_PER_SESSION = 100
-slice_size = 10000                                           # Memory management, None for no slicing
+slice_size = 10000                                           # Memory management, None for entire dataset
 non_text_cols = ['id', 'locale', 'price']
 
 
 # Function to load all data
 def load_all_data():
+    """
+    This function loads and cleans all necessary data for the project.
+    It calls the load_and_clean_data function for each dataset and returns them as a tuple.
+    """
     # Load and clean data
     processed_file = train_path + '/products_train_processed.csv'
     products_train = load_and_clean_data(train_path + '/products_train.csv', is_products=True, processed_file=processed_file, slice_size=slice_size)
